f2965d1e21bf7b4ed0b3185894be071c42cd356a
Mustafa Emre Ilhan <mustafa.emre@btidanismanlik.com>
2020-11-08 17:28:09 +0300

    sql eklendi

--- application/views/Ekle.php
+++ application/views/Ekle.php
@@ -10,7 +10,7 @@
 
 	<form action="<?php echo base_url("Liste/insert") ?>" method="post"> 
 
-	<span> İsim = </span> <input type="text" name="isim" placeholder="isim Giriniz"> <br>
+	<span> İsim = </span> <input type="text" name="isim" placeholder="isim Giriniz" align="right"> <br>
 	<span> Soyisim = </span> <input type="text" name="soyisim" placeholder="soyisim Giriniz"> <br>
 	<span> Yas = </span> <input type="text" name="yas" placeholder="yas Giriniz"> <br>
 	<span> Mail = </span> <input type="email" name="mail" placeholder="mail Giriniz"> <br><br>
--- db/personel.sql
+++ db/personel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 04 Kas 2020, 13:01:54
+-- Üretim Zamanı: 08 Kas 2020, 15:26:53
 -- Sunucu sürümü: 10.4.14-MariaDB
 -- PHP Sürümü: 7.2.34
 
@@ -29,23 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `personel` (
   `id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
-  `detail` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL
+  `isim` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `soyisim` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
+  `yas` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8_turkish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
 -- Tablo döküm verisi `personel`
 --
 
-INSERT INTO `personel` (`id`, `title`, `detail`) VALUES
-(1, 'test', ' 		bjk	 		'),
-(3, 'deneme', 'fb'),
-(4, 'uyguluma', 'gs'),
-(14, 'controller', 'merhaba	'),
-(15, 'test test', 'sdadsfasd'),
-(16, 'asdfasd', 'fbsdafsadf'),
-(17, 'afdsdsaf', 'gsdfsads'),
-(18, 'viev', 'merhabafadfdsf');
+INSERT INTO `personel` (`id`, `isim`, `soyisim`, `yas`, `mail`) VALUES
+(1, 'Ahmet', 'Test', '21', 'Ahmet@test.com'),
+(19, 'Mehmet', 'kıvırcık ', '21', 'Mehmet@test.com'),
+(23, 'ayşe', 'artmut', '60', 'ayse@test.com'),
+(24, 'aslı', 'test', '20', 'aslı@test.com'),
+(26, 'sedat', 'hakan', '22', 'sedat@test.com'),
+(27, 'akın', 'tasan', '21', 'akın@test.com'),
+(28, 'safak', 'mehmet', '40', 'safakmehmet@hotmail.com'),
+(29, 'ahmet', 'kavakçı', '50', 'ahmetkavakci@hotmail.com');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -65,7 +67,7 @@ ALTER TABLE `personel`
 -- Tablo için AUTO_INCREMENT değeri `personel`
 --
 ALTER TABLE `personel`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
